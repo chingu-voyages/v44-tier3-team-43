8ba8a05d2c58a6a866f795c8c8548d3028b03ef6
K0D0D <kovach.daniil.js@gmail.com>
2023-05-11 18:11:29 +0200

    finish project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,35 @@
 /** @type {import('tailwindcss').Config} */
+
+const { fontFamily } = require("tailwindcss/defaultTheme");
+const { colors } = require("tailwindcss/colors");
+
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}"
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: "1.25rem",
+		},
+		extend: {
+			fontFamily: {
+				sans: ["var(--font-inter)", ...fontFamily.sans]
+			},
+			colors: {
+				...colors,
+				"custom-black": "#182326",
+				"custom-white": "#E7E7E7",
+				"dark-grey": "#273032",
+				"light-grey": "#A8A8A8",
+				"custom-red": "#920D0D",
+				"custom-green": "#177807",
+				"custom-blue": "#12506B",
+				"custom-orange": "#BC5D19"
+			}
+		}
+	},
+	plugins: []
+};
