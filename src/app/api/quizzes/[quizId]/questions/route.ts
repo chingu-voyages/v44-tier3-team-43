03b1395b42a1608c05966b0bd0879e1a18742200
K0D0D <kovach.daniil.js@gmail.com>
2023-07-05 20:19:26 +0200

    add additional api validation

--- src/app/api/quizzes/[quizId]/questions/route.ts
+++ src/app/api/quizzes/[quizId]/questions/route.ts
@@ -5,152 +5,171 @@ import { questionSchema } from "@/utils/schemas";
 import { prisma } from "@/lib/prisma";
 
 export const GET = async (
-    req: NextRequest,
-    { params: { quizId } }: { params: { quizId: string } }
+	req: NextRequest,
+	{ params: { quizId } }: { params: { quizId: string } }
 ) => {
-    try {
-        const questions = await prisma.question.findMany({
-            where: {
-                quizId: quizId
-            },
-            include: {
-                answers: true
-            }
-        });
+	try {
+		const quiz = await prisma.quiz.findUnique({
+			where: {
+				id: quizId
+			}
+		});
 
-        return NextResponse.json(questions, {
-            status: 200
-        });
-    } catch (err) {
-        if (err instanceof z.ZodError) {
-            return NextResponse.json(
-                {
-                    error: err.issues
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		if (!quiz) {
+			return NextResponse.json(
+				{
+					error: {
+						message: `Invalid request: Quiz with id '${quizId}' doesn't exist`
+					}
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        return NextResponse.json(
-            {
-                error: "Internal Server Error"
-            },
-            {
-                status: 500
-            }
-        );
-    }
+		const questions = await prisma.question.findMany({
+			where: {
+				quizId: quizId
+			},
+			include: {
+				answers: true
+			}
+		});
+
+		return NextResponse.json(questions, {
+			status: 200
+		});
+	} catch (err) {
+		if (err instanceof z.ZodError) {
+			return NextResponse.json(
+				{
+					error: err.issues
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		return NextResponse.json(
+			{
+				error: "Internal Server Error"
+			},
+			{
+				status: 500
+			}
+		);
+	}
 };
 
 export const POST = async (
-    req: NextRequest,
-    { params: { quizId } }: { params: { quizId: string } }
+	req: NextRequest,
+	{ params: { quizId } }: { params: { quizId: string } }
 ) => {
-    try {
-        const user = await getUserSession().then((session) => session?.user);
+	try {
+		const user = await getUserSession().then((session) => session?.user);
 
-        if (!user) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "Unauthorized to perform this action"
-                    }
-                },
-                {
-                    status: 401
-                }
-            );
-        }
+		if (!user) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "Unauthorized to perform this action"
+					}
+				},
+				{
+					status: 401
+				}
+			);
+		}
 
-        const body = await req.json();
+		const body = await req.json();
 
-        const response = questionSchema.safeParse(body);
+		const response = questionSchema.safeParse(body);
 
-        if (!response.success) {
-            const { errors } = response.error;
+		if (!response.success) {
+			const { errors } = response.error;
 
-            return NextResponse.json(
-                {
-                    error: { message: "Invalid request", errors }
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+			return NextResponse.json(
+				{
+					error: { message: "Invalid request", errors }
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        const { title, answers } = response.data;
+		const { title, answers } = response.data;
 
-        const quiz = await prisma.quiz.findUnique({
-            where: {
-                id: quizId
-            }
-        });
+		const quiz = await prisma.quiz.findUnique({
+			where: {
+				id: quizId
+			}
+		});
 
-        if (!quiz) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: `Invalid request: Quiz with id '${quizId}' doesn't exist`
-                    }
-                },
-                {
-                    status: 400
-                }
-            );
-        } else if (quiz.userId !== user.id) {
-            return NextResponse.json(
-                {
-                    error: { message: "Forbidden" }
-                },
-                {
-                    status: 403
-                }
-            );
-        }
+		if (!quiz) {
+			return NextResponse.json(
+				{
+					error: {
+						message: `Invalid request: Quiz with id '${quizId}' doesn't exist`
+					}
+				},
+				{
+					status: 400
+				}
+			);
+		} else if (quiz.userId !== user.id) {
+			return NextResponse.json(
+				{
+					error: { message: "Forbidden" }
+				},
+				{
+					status: 403
+				}
+			);
+		}
 
-        const data = await prisma.question.create({
-            data: {
-                quizId,
-                title,
-                answers: {
-                    createMany: {
-                        data: answers.map((answer) => ({
-                            text: answer.text,
-                            correct: answer.correct
-                        }))
-                    }
-                }
-            },
-            include: {
-                answers: true
-            }
-        });
+		const data = await prisma.question.create({
+			data: {
+				quizId,
+				title,
+				answers: {
+					createMany: {
+						data: answers.map((answer) => ({
+							text: answer.text,
+							correct: answer.correct
+						}))
+					}
+				}
+			},
+			include: {
+				answers: true
+			}
+		});
 
-        return NextResponse.json(data, {
-            status: 200
-        });
-    } catch (err) {
-        if (err instanceof z.ZodError) {
-            return NextResponse.json(
-                {
-                    error: err.issues
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		return NextResponse.json(data, {
+			status: 200
+		});
+	} catch (err) {
+		if (err instanceof z.ZodError) {
+			return NextResponse.json(
+				{
+					error: err.issues
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        return NextResponse.json(
-            {
-                error: "Internal Server Error"
-            },
-            {
-                status: 500
-            }
-        );
-    }
+		return NextResponse.json(
+			{
+				error: "Internal Server Error"
+			},
+			{
+				status: 500
+			}
+		);
+	}
 };
--- src/app/api/quizzes/[quizId]/route.ts
+++ src/app/api/quizzes/[quizId]/route.ts
@@ -5,253 +5,285 @@ import { quizSchema } from "@/utils/schemas";
 import { prisma } from "@/lib/prisma";
 
 export const GET = async (
-    req: NextRequest,
-    { params: { quizId } }: { params: { quizId: string } }
+	req: NextRequest,
+	{ params: { quizId } }: { params: { quizId: string } }
 ) => {
-    try {
-        const quiz = await prisma.quiz.findUnique({
-            where: {
-                id: quizId
-            },
-            include: {
-                _count: true
-            }
-        });
+	try {
+		const quiz = await prisma.quiz.findUnique({
+			where: {
+				id: quizId
+			},
+			include: {
+				_count: true
+			}
+		});
 
-        if (!quiz) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "No quiz found!"
-                    }
-                },
-                {
-                    status: 404
-                }
-            );
-        }
+		if (!quiz) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "No quiz found!"
+					}
+				},
+				{
+					status: 404
+				}
+			);
+		}
 
-        return NextResponse.json(quiz, {
-            status: 200
-        });
-    } catch (err) {
-        if (err instanceof z.ZodError) {
-            return NextResponse.json(
-                {
-                    error: err.issues
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		return NextResponse.json(quiz, {
+			status: 200
+		});
+	} catch (err) {
+		if (err instanceof z.ZodError) {
+			return NextResponse.json(
+				{
+					error: err.issues
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        return NextResponse.json(
-            {
-                error: "Internal Server Error"
-            },
-            {
-                status: 500
-            }
-        );
-    }
+		return NextResponse.json(
+			{
+				error: "Internal Server Error"
+			},
+			{
+				status: 500
+			}
+		);
+	}
 };
 
 export const PUT = async (
-    req: NextRequest,
-    { params: { quizId } }: { params: { quizId: string } }
+	req: NextRequest,
+	{ params: { quizId } }: { params: { quizId: string } }
 ) => {
-    try {
-        const user = await getUserSession().then((session) => session?.user);
+	try {
+		const user = await getUserSession().then((session) => session?.user);
 
-        if (!user) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "Unauthorized to perform this action!"
-                    }
-                },
-                {
-                    status: 401
-                }
-            );
-        }
+		if (!user) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "Unauthorized to perform this action!"
+					}
+				},
+				{
+					status: 401
+				}
+			);
+		}
 
-        if (!quizId) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "Something went wrong! Check if parameters are correct!"
-                    }
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		if (!quizId) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "Something went wrong! Check if parameters are correct!"
+					}
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        const quiz = await prisma.quiz.findUnique({
-            where: {
-                id: quizId
-            }
-        });
+		const quiz = await prisma.quiz.findUnique({
+			where: {
+				id: quizId
+			}
+		});
 
-        if (quiz?.userId !== user.id) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "Unauthorized to perform this action!"
-                    }
-                },
-                {
-                    status: 401
-                }
-            );
-        }
+		if (quiz?.userId !== user.id) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "Forbidden"
+					}
+				},
+				{
+					status: 403
+				}
+			);
+		}
 
-        const body = await req.json();
+		const body = await req.json();
 
-        const response = quizSchema.safeParse(body);
+		const response = quizSchema.safeParse(body);
 
-        if (!response.success) {
-            const { errors } = response.error;
+		if (!response.success) {
+			const { errors } = response.error;
 
-            return NextResponse.json(
-                {
-                    error: { message: "Invalid request", errors }
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+			return NextResponse.json(
+				{
+					error: { message: "Invalid request", errors }
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        const { category, title, image } = response.data;
+		const { category, title, image } = response.data;
 
-        const data = await prisma.quiz.update({
-            where: {
-                id: quizId
-            },
-            data: {
-                category,
-                title,
-                image
-            }
-        });
+		const dbCategory = await prisma.category.findFirst({
+			where: {
+				name: {
+					equals: category,
+					mode: "insensitive"
+				}
+			}
+		});
 
-        return NextResponse.json(data, {
-            status: 200
-        });
-    } catch (err) {
-        if (err instanceof z.ZodError) {
-            return NextResponse.json(
-                {
-                    error: err.issues
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		if (!dbCategory) {
+			return NextResponse.json(
+				{
+					error: {
+						message: `Invalid request: Category '${category}' doesn't exist`
+					}
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        return NextResponse.json(
-            {
-                error: "Internal Server Error"
-            },
-            {
-                status: 500
-            }
-        );
-    }
+		const data = await prisma.quiz.update({
+			where: {
+				id: quizId
+			},
+			data: {
+				category: dbCategory.name,
+				title,
+				image
+			}
+		});
+
+		return NextResponse.json(data, {
+			status: 200
+		});
+	} catch (err) {
+		if (err instanceof z.ZodError) {
+			return NextResponse.json(
+				{
+					error: err.issues
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		return NextResponse.json(
+			{
+				error: "Internal Server Error"
+			},
+			{
+				status: 500
+			}
+		);
+	}
 };
 
 export const DELETE = async (
-    req: NextRequest,
-    { params: { quizId } }: { params: { quizId: string } }
+	req: NextRequest,
+	{ params: { quizId } }: { params: { quizId: string } }
 ) => {
-    try {
-        const user = await getUserSession().then((session) => session?.user);
+	try {
+		const user = await getUserSession().then((session) => session?.user);
 
-        if (!user) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "Unauthorized to perform this action!"
-                    }
-                },
-                {
-                    status: 401
-                }
-            );
-        }
+		if (!user) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "Unauthorized to perform this action!"
+					}
+				},
+				{
+					status: 401
+				}
+			);
+		}
 
-        if (!quizId) {
-            return NextResponse.json(
-                {
-                    error: {
-                        message: "Something went wrong! Check if parameters are correct!"
-                    }
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		if (!quizId) {
+			return NextResponse.json(
+				{
+					error: {
+						message: "Something went wrong! Check if parameters are correct!"
+					}
+				},
+				{
+					status: 400
+				}
+			);
+		}
 
-        const quiz = await prisma.quiz.findUnique({
-            where: {
-                id: quizId
-            }
-        });
+		const quiz = await prisma.quiz.findUnique({
+			where: {
+				id: quizId
+			}
+		});
 
-        if (quiz?.userId !== user.id) {
-            return NextResponse.json(
-                {
-                    error: { 
-                        message: "Unauthorized to perform this action!"
-                    }
-                },
-                {
-                    status: 401
-                }
-            );
-        } else {
-            await prisma.quiz.delete({
-                where: {
-                    id: quizId
-                }
-            });
-        }
+		if (!quiz) {
+			return NextResponse.json(
+				{
+					error: {
+						message: `Invalid request: Quiz with id '${quizId}' doesn't exist`
+					}
+				},
+				{
+					status: 400
+				}
+			);
+		} else if (quiz.userId !== user.id) {
+			return NextResponse.json(
+				{
+					error: { message: "Forbidden" }
+				},
+				{
+					status: 403
+				}
+			);
+		}
 
-        return NextResponse.json(
-            {
-                data: {
-                    message: "The selected quiz and related questions has been successfully deleted!"
-                }
-            },
-            {
-                status: 200
-            }
-        );
-    } catch (err) {
-        if (err instanceof z.ZodError) {
-            return NextResponse.json(
-                {
-                    error: err.issues
-                },
-                {
-                    status: 400
-                }
-            );
-        }
+		await prisma.quiz.delete({
+			where: {
+				id: quizId
+			}
+		});
 
-        return NextResponse.json(
-            {
-                error: "Internal Server Error"
-            },
-            {
-                status: 500
-            }
-        );
-    }
+		return NextResponse.json(
+			{
+				data: {
+					message:
+						"The selected quiz and related questions have been successfully deleted!"
+				}
+			},
+			{
+				status: 200
+			}
+		);
+	} catch (err) {
+		if (err instanceof z.ZodError) {
+			return NextResponse.json(
+				{
+					error: err.issues
+				},
+				{
+					status: 400
+				}
+			);
+		}
+
+		return NextResponse.json(
+			{
+				error: "Internal Server Error"
+			},
+			{
+				status: 500
+			}
+		);
+	}
 };
