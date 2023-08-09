0cb94d7d7c920bcec7b08c9ee23f88092dd8bd2f
Pio-js <pio.saija@web.de>
2023-08-09 15:44:28 +0200

    add my quizzes endpoint

---
+++ src/app/api/quizzes/my/route.ts
@@ -0,0 +1,44 @@
+import { getUserSession } from "@/lib/auth";
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+export const GET = async (req: NextRequest) => {
+    try {
+        const user = await getUserSession().then((session) => session?.user);
+
+        if (!user) {
+            return NextResponse.json("Unauthorized to perform this action", {
+                status: 401
+            });
+        }
+
+        const page = Number(req.nextUrl.searchParams.get("page")) || 1;
+        const perPage = Number(req.nextUrl.searchParams.get("per_page")) || 20;
+
+        const data = await prisma.quiz.findMany({
+            where: {
+                userId: user.id
+            },
+            orderBy: {
+                createdAt: "desc"
+            },
+            skip: (page - 1) * perPage,
+            take: perPage
+        });
+
+        return NextResponse.json(data, {
+            status: 200
+        });
+    } catch (err) {
+        if (err instanceof z.ZodError) {
+            return NextResponse.json(generateZodMessage(err.issues), {
+                status: 400
+            });
+        }
+
+        return NextResponse.json("Internal Server Error", {
+            status: 500
+        });
+    }
+};
